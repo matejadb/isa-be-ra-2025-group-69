16d4048ae4290410cb6a21f1900f96d29c675372
matejadb <92729211+matejadb@users.noreply.github.com>
2025-12-27 15:33:45 +0100

    feat: add dependencies and implement User class

---
+++ src/main/java/com/project/backend/model/User.java
@@ -0,0 +1,58 @@
+package com.project.backend.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.engine.internal.ImmutableEntityEntry;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    @Email(message = "Email needs to be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @Column(nullable = false)
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 6 characters")
+    private String password;
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+
+    @Column(nullable = false)
+    private boolean activated = false;
+
+    @Column(unique = true)
+    private String activationToken;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
